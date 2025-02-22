2b3fdd21a0e269ec52551cb2fb245d3f23109ceb
Hadi Faraz <hadifaraz52@protonmail.com>
2025-02-22 21:34:55 +0000

    what the fuck but pretty

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -3,6 +3,46 @@ use std::ops::Add;
 /// Returns a `String` containing the LaTeX code for the specified table
 ///
 /// `output_vals` must have exactly `2^input_cols` rows, otherwise this will return an error
-pub fn generate_table(input_cols: i32, output_vals: &mut Vec<Vec<i32>>, col_names: Vec<&str>) -> String {
-    format!("\\begin{{table*}}{{}}\n    \\begin{{tabular}}{{ {} }}\n        {} \\\\ \n        \\hline \n{}\n    \\end{{tabular}}\n\\end{{table*}}", (0..(input_cols + output_vals[0].len() as i32)).enumerate().map(|(i, _)| if i == input_cols as usize { "| c" } else { " c" }).collect::<Vec<&str>>().join(" |").as_str(), col_names.join(" & ").as_str(), (0..2_i32.pow(input_cols as u32)).map(|n| format!("{n:0amount$b}", amount = input_cols as usize).trim().chars().map(|c| c.to_string()).collect::<Vec<String>>().join(" & ").add(" & ").add(output_vals.get(n as usize).unwrap().iter().map(|n| n.to_string()).collect::<Vec<String>>().join(" & ").as_str())).map(|v|String::from("        ").add(v.to_string().as_str())).collect::<Vec<String>>().join(" \\\\ \n").as_str())
+pub fn generate_table(
+    input_cols: i32,
+    output_vals: &mut Vec<Vec<i32>>,
+    col_names: Vec<&str>,
+) -> String {
+    format!(
+        "\\begin{{table*}}{{}}\n    \\begin{{tabular}}{{ {} }}\n        {} \\\\ \n        \\hline \n{}\n    \\end{{tabular}}\n\\end{{table*}}",
+        (0..(input_cols + output_vals[0].len() as i32))
+            .enumerate()
+            .map(
+                |(i, _)| if i == input_cols as usize { "| c" } else { " c" }
+            ).collect::<Vec<&str>>()
+            .join(" |")
+            .as_str(),
+
+        col_names.join(" & ").as_str(),
+
+        (0..2_i32.pow(input_cols as u32))
+            .map(
+                |n| format!(
+                    "{n:0amount$b}", amount = input_cols as usize
+                ).trim()
+                    .chars()
+                    .map(|c| c.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" & ")
+                    .add(" & ")
+                    .add(
+                        output_vals.get(n as usize)
+                            .unwrap()
+                            .iter()
+                            .map(|n| n.to_string())
+                            .collect::<Vec<String>>()
+                            .join(" & ")
+                            .as_str()
+                    )
+            )
+            .map(|v| String::from("        ").add(v.as_str()))
+            .collect::<Vec<String>>()
+            .join(" \\\\ \n")
+            .as_str()
+    )
 }
